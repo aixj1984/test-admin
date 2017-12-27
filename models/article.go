98249fdc03d520c70e78498b00d7421a5298b205
unknown <richard.ai@blackshark.com>
2017-12-27 19:30:31 +0800

    add article

--- models/article.go
+++ models/article.go
@@ -5,9 +5,9 @@ package models
 type Article struct {
 	Id         int
 	Title      string `xorm:"varchar(300) 'title'"`
-	Content    string `xorm:"text 'content'"`
+	Content    string `xorm:"longtext 'content'"`
 	Source     string `xorm:"varchar(300) 'source'"`
-	Abstract   string `xorm:"text   'abstract'"`
+	Abstract   string `xorm:"longtext   'abstract'"`
 	PublicTime string `xorm:"varchar(60)  'public_time'"`
 	ReadCount  int    `xorm:"int  'read_count'"`
 	Status     int8   `xorm:"int  'status'"`
